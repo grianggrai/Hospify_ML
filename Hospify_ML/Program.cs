3cb3e12da3af7abb9d99ee0d0c525121d5b6c028
Kriengkrai Ritthaphrom <grianggrai@gmail.com>
2023-11-19 00:19:02 +0700

    Add project files.

---
+++ Hospify_ML/Program.cs
@@ -0,0 +1,28 @@
+﻿using Hospify_ML;
+
+namespace Hospify_ML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ML.NET!");
+            // Add input data
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = "This restaurant was wonderful."
+            };
+
+            // Load model and predict output of sample data
+            var result = SentimentModel.Predict(sampleData);
+
+            // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+            var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+        }
+
+    }
+}
+
+
+
---
+++ WebAPI/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WebAPI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
